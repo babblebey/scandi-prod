6c116c0fe68b70d8141a6c4b8f0ae56d0cbc8b8a
babblebey <babblebey@gmail.com>
2023-05-24 20:39:00 +0100

    commit server/bootstrap

---
+++ server/bootstrap.php
@@ -0,0 +1,18 @@
+<?php 
+require 'vendor/autoload.php';
+
+use Src\Config\Database;
+use Src\Handlers\ProductHandler;
+
+// $db = (new Database())->connect();
+
+// $productHandle = new ProductHandler($db);
+
+// // print_r($productHandle->findAll());
+
+// $productHandle->insert([
+//     'sku' => 'hfngn-fff-fff',
+//     'name' => 'Latest Shoes',
+//     'price' => 20.3,
+//     'attribute' => 700
+// ], 'Book');
