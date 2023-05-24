b5701a788d6114315fc9a18c9ec73269ae74b688
babblebey <babblebey@gmail.com>
2023-05-24 20:29:14 +0100

    handlers/ProductHandler initial commit with findAll() and insert() methods ready

---
+++ server/src/handlers/ProductHandler.php
@@ -0,0 +1,77 @@
+<?php 
+namespace Src\Handlers;
+
+use Src\Interfaces\ProductHandlerInterface;
+
+class ProductHandler implements ProductHandlerInterface {
+    const TABLE = 'product';
+
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getInstance($productType, array $params) {
+        $class = 'Src\\Models\\' . $productType;
+        return new $class($params['sku'], $params['name'], $params['price'], $params['attribute']);
+    }
+
+    public function findAll() {
+        $query = '
+            SELECT 
+                * 
+            FROM 
+                '. self::TABLE .'
+        ';
+        try {
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            return $result;
+        } catch (\PDOExcerption $error) {
+            exit("Product Retrieval Error: " . $error->getMessage());
+        }
+        
+    }
+
+    public function insert(array $productDetails, $productType) {
+        print_r($productType);
+
+        $product = $this->getInstance($productType, $productDetails);
+        $productAttributeKey = key($product->getSpecialAttribute());
+
+        echo $productAttributeKey;
+
+        $productQuery = $product->getInsertQuery();
+        $localQuery = '
+            INSERT INTO '. self::TABLE .' 
+                (sku, name, price)
+            VALUES
+                (:sku, :name, :price)
+        ';
+        
+        try {
+            $productStmt = $this->db->prepare($productQuery);
+            $productStmt->execute([
+                'sku' => $productDetails['sku'],
+                $productAttributeKey => $productDetails['attribute']
+            ]);
+
+            $localStmt = $this->db->prepare($localQuery);
+            $localStmt->execute([
+                'sku' => $productDetails['sku'],
+                'name' => $productDetails['name'],
+                'price' =>  $productDetails['price']
+            ]);
+
+            echo 'Product Inserted Successully';
+        } catch (\PDOExcerption $error) {
+            exit("Product Insertion Error: " . $error->getMessage());
+        }
+    }
+
+    public function delete() {
+
+    }
+}
