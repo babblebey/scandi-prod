ac4589847cda57d850653d410459940c42df681e
babblebey <babblebey@gmail.com>
2023-05-31 14:55:56 +0100

    refactored: abstracted all Home page/route functions/states to app's MainContext

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import { FC, useState, useEffect } from 'react';
+import { FC, useContext } from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,85 +8,15 @@ import Button from "react-bootstrap/Button";
 import PlusIcon from "../icons/PlusIcon";
 import TrashIcon from "../icons/TrashIcon";
 import ProductCard from "../components/ProductCard";
-import axios from "axios";
-
-import type { Product } from "../types";
+import { MainContext } from "../context/MainContext";
+import { Product, AppContext } from "../types";
 
 interface HomeProps {
     
 }
 
 const Home: FC<HomeProps> = () => {
-    const [selectedProductSKUs, setSelectedProductSKUs] = useState<string[]>([]);
-    const [products, setProducts] = useState<Product[]>();
-
-    useEffect(() => {
-        // Make GET request to retrieve products
-        axios.get('http://127.0.0.1:8000/products')
-        .then(response => {
-            setProducts(response.data.data);
-        })
-        .catch(error => {
-            console.error('Error retrieving products:', error);
-        });
-    }, []);
-
-    const handleDelete = (): void => {
-        axios.delete('http://127.0.0.1:8000/products', {
-            data: {
-                skus: selectedProductSKUs
-            }
-        })
-        .then(response => {
-            // If response is OK
-            if (response.status === 200) {
-                // Filter delected products from products using selectedProductSKUs
-                const updatedProducts = products?.filter(
-                    product => !selectedProductSKUs.includes(product.sku)
-                );
-
-                // Reset selectedProductSKUs to empty array
-                setSelectedProductSKUs([]);
-
-                // update products with filtered - updatedProducts
-                setProducts(updatedProducts);
-            } else {
-                throw new Error(response.data.error);
-            }
-        })
-        .catch(error => {
-            console.error('Error deleting products:', error);
-        });
-    }
-
-    // Product Select Handler
-    const handleSelectProduct = (sku: string): void => {
-        // Create a Copy
-        const updatedSelectedProductSKUs = [...selectedProductSKUs];
-
-        // Check if the productSKU is already selected - return the index in array or -1
-        const index = updatedSelectedProductSKUs.findIndex(
-            currentSelectedProductSKU => currentSelectedProductSKU === sku
-        );
-
-        // ProductSKU is 
-        if (index < 0) {
-            // ---not selected, add it to the selection
-            updatedSelectedProductSKUs.push(sku);
-        } else {
-            // ---already selected, remove it from the selection
-            updatedSelectedProductSKUs.splice(index, 1);
-        }
-        // Set New 
-        setSelectedProductSKUs(updatedSelectedProductSKUs);
-    }
-
-    // Check If Product is Seleted
-    const isSelected = (sku: string): boolean => {
-        return selectedProductSKUs.includes(sku);
-    }
-
-    // console.log(selectedProductSKUs);
+    const { products, selectedProductSKUs, isSelected, handleSelectProduct, handleDelete } = useContext(MainContext) as AppContext;
 
     return ( 
         <>
@@ -97,7 +27,7 @@ const Home: FC<HomeProps> = () => {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Nav>
-                        { !!(selectedProductSKUs.length) && (
+                        { !!(selectedProductSKUs?.length) && (
                             // Render Delete Button when a Product is selected
                             <Button variant="danger" className="me-4 shadow"
                                 onClick={() => handleDelete()}
@@ -127,7 +57,7 @@ const Home: FC<HomeProps> = () => {
 
             <Container>
                 <Row lg="4" md="3" sm="2" xs="1">
-                    { products && products.map((data, i) => (
+                    { products && products.map((data: Product, i: number) => (
                         <ProductCard key={i} data={data} isSelected={isSelected} handleSelect={handleSelectProduct}/>
                     )) }
                 </Row>
