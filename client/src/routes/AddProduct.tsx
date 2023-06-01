e63cb2bb7c09c13d38744e9fcf8f9c05b3d45f55
babblebey <babblebey@gmail.com>
2023-06-01 21:30:13 +0100

    feat: implemented productType switching on AddProduct page/route

--- client/src/routes/AddProduct.tsx
+++ client/src/routes/AddProduct.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,18 +11,21 @@ import InputGroup from "react-bootstrap/InputGroup";
 import CheckIcon from "../icons/CheckIcon";
 import CancelIcon from "../icons/CancelIcon";
 import { Link } from "react-router-dom";
-import type { FormEvent } from 'react';
+import type { FormEvent, ChangeEvent } from 'react';
+import type { ProductType } from "../types/Product";
 
 interface AddProductProps {
     
 }
  
 const AddProduct: FC<AddProductProps> = () => {
+    const [productType, setProductType] = useState<ProductType>();
+
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
 
         // Testing Submit Action
-        console.log('Form Submitted!');   
+        console.log('Form Submitted!');
     }
 
     return ( 
@@ -95,8 +98,13 @@ const AddProduct: FC<AddProductProps> = () => {
                     {/* Product Type Switcher */}
                     <Row>
                         <Form.Label htmlFor="productType">Product Type</Form.Label>
-                        <Form.Select id="productType">
-                            <option disabled>Select Product Type</option>
+                        <Form.Select id="productType" 
+                            defaultValue="" 
+                            onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                                setProductType(e.target.value as ProductType)
+                            } }
+                        >
+                            <option value="" disabled>Select Product Type</option>
                             <option value="Book">Book</option>
                             <option value="DVD">DVD</option>
                             <option value="Furniture">Furniture</option>
@@ -104,87 +112,93 @@ const AddProduct: FC<AddProductProps> = () => {
                     </Row>
 
                     {/* Attribute for ProductType - Book */}
-                    <Row>
-                        <Form.Label htmlFor="weight">Weight</Form.Label>
-                        <InputGroup>
-                            <Form.Control
-                                type="number"
-                                id="weight"
-                                aria-label="Book Weight (in kilograms)"
-                                aria-describedby="weightDescriptionBlock"
-                            />
-                            <InputGroup.Text>KG</InputGroup.Text>
-                        </InputGroup>
-                        <Form.Text id="weightDescriptionBlock" muted>
-                            Please, provide Book's weight in kilograms (KG).
-                        </Form.Text>
-                    </Row>
+                    { (productType === "Book") && (
+                        <Row>
+                            <Form.Label htmlFor="weight">Weight</Form.Label>
+                            <InputGroup>
+                                <Form.Control
+                                    type="number"
+                                    id="weight"
+                                    aria-label="Book Weight (in kilograms)"
+                                    aria-describedby="weightDescriptionBlock"
+                                />
+                                <InputGroup.Text>Kilograms (KG)</InputGroup.Text>
+                            </InputGroup>
+                            <Form.Text id="weightDescriptionBlock" muted>
+                                Please, provide Book's weight in kilograms (KG).
+                            </Form.Text>
+                        </Row>
+                    ) }
                     
                     {/* Attribute for ProductType - DVD */}
-                    <Row>
-                        <Form.Label htmlFor="size">Size</Form.Label>
-                        <InputGroup>
-                            <Form.Control
-                                type="number"
-                                id="size"
-                                aria-label="DVD Size (in megabytes)"
-                                aria-describedby="sizeDescriptionBlock"
-                            />
-                            <InputGroup.Text>MB</InputGroup.Text>
-                        </InputGroup>
-                        <Form.Text id="sizeDescriptionBlock" muted>
-                            Please, provide DVD's size in megabytes (MB).
-                        </Form.Text>
-                    </Row>
+                    { (productType === "DVD") && (
+                        <Row>
+                            <Form.Label htmlFor="size">Size</Form.Label>
+                            <InputGroup>
+                                <Form.Control
+                                    type="number"
+                                    id="size"
+                                    aria-label="DVD Size (in megabytes)"
+                                    aria-describedby="sizeDescriptionBlock"
+                                />
+                                <InputGroup.Text>Megabytes (MB)</InputGroup.Text>
+                            </InputGroup>
+                            <Form.Text id="sizeDescriptionBlock" muted>
+                                Please, provide DVD's size in megabytes (MB).
+                            </Form.Text>
+                        </Row>
+                    ) }
 
                     {/* Attribute for ProductType - Furniture */}
-                    <Row>
-                        <Form.Label htmlFor="size">Dimension</Form.Label>
-                        <Row lg="3" md="2" sm="1" xs="1" className="p-0 m-0">
-                            <Col className="pb-lg-0 pb-3 pe-lg-4 px-0">
-                                <InputGroup>
-                                    <InputGroup.Text id="heightDescriptionBlock">
-                                        Height
-                                    </InputGroup.Text>
-                                    <Form.Control
-                                        type="number"
-                                        id="height"
-                                        aria-label="Furniture Height"
-                                        aria-describedby="heightDescriptionBlock"
-                                    />
-                                </InputGroup>
-                            </Col>
-                            <Col className="pb-lg-0 pb-3 pe-lg-4 px-0">
-                                <InputGroup>
-                                    <InputGroup.Text id="widthDescriptionBlock">
-                                        Width
-                                    </InputGroup.Text>
-                                    <Form.Control
-                                        type="number"
-                                        id="width"
-                                        aria-label="Furniture Width"
-                                        aria-describedby="widthDescriptionBlock"
+                    { (productType === "Furniture") && (
+                        <Row>
+                            <Form.Label htmlFor="size">Dimension</Form.Label>
+                            <Row lg="3" md="2" sm="1" xs="1" className="p-0 m-0">
+                                <Col className="pb-lg-0 pb-3 pe-lg-4 px-0">
+                                    <InputGroup>
+                                        <InputGroup.Text id="heightDescriptionBlock">
+                                            Height
+                                        </InputGroup.Text>
+                                        <Form.Control
+                                            type="number"
+                                            id="height"
+                                            aria-label="Furniture Height"
+                                            aria-describedby="heightDescriptionBlock"
                                         />
-                                </InputGroup>
-                            </Col>
-                            <Col className="px-0">
-                                <InputGroup>
-                                    <InputGroup.Text id="lengthDescriptionBlock">
-                                        Length
-                                    </InputGroup.Text>
-                                    <Form.Control
-                                        type="number"
-                                        id="length"
-                                        aria-label="Furniture Length"
-                                        aria-describedby="lengthDescriptionBlock"
-                                    />
-                                </InputGroup>
-                            </Col>
+                                    </InputGroup>
+                                </Col>
+                                <Col className="pb-lg-0 pb-3 pe-lg-4 px-0">
+                                    <InputGroup>
+                                        <InputGroup.Text id="widthDescriptionBlock">
+                                            Width
+                                        </InputGroup.Text>
+                                        <Form.Control
+                                            type="number"
+                                            id="width"
+                                            aria-label="Furniture Width"
+                                            aria-describedby="widthDescriptionBlock"
+                                            />
+                                    </InputGroup>
+                                </Col>
+                                <Col className="px-0">
+                                    <InputGroup>
+                                        <InputGroup.Text id="lengthDescriptionBlock">
+                                            Length
+                                        </InputGroup.Text>
+                                        <Form.Control
+                                            type="number"
+                                            id="length"
+                                            aria-label="Furniture Length"
+                                            aria-describedby="lengthDescriptionBlock"
+                                        />
+                                    </InputGroup>
+                                </Col>
+                            </Row>
+                            <Form.Text id="sizeDescriptionBlock" muted>
+                                Please, provide Furniture's dimension in Height x Width x Length (HxWxL).
+                            </Form.Text>
                         </Row>
-                        <Form.Text id="sizeDescriptionBlock" muted>
-                            Please, provide Furniture's dimension in Height x Width x Length (HxWxL).
-                        </Form.Text>
-                    </Row>
+                    ) }
                 </Form>
             </Container>
         </>
